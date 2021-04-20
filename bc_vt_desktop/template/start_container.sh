700b672fc5dd912479bf63b886246089239f7939
rsettlage <rsettlag@vt.edu>
2021-04-20 17:43:27 -0400

    removed debug flag from desktop start

--- bc_vt_desktop/template/start_container.sh
+++ bc_vt_desktop/template/start_container.sh
@@ -4,4 +4,4 @@ echo using $1
 ##singularity run --bind=/home/$USER:/home/$USER,/var:/var,`pwd`/vnc.log:/dockerstartup/vnc.log,`pwd`/novnc.log:/dockerstartup/novnc.log,/tmp:/tmp,/projects:/projects,/work:/work,`pwd`/startup.sh:/dockerstartup/startup.sh,`pwd`/port.rc:/dockerstartup/port.rc --writable-tmpfs --containall $1 --debug
 
 
-singularity run --bind=/home/$USER:/home/$USER/Documents,/var:/var,`pwd`/vnc.log:/dockerstartup/vnc.log,`pwd`/novnc.log:/dockerstartup/novnc.log,/tmp:/tmp,/projects:/projects,/work:/work,`pwd`/startup.sh:/dockerstartup/startup.sh,`pwd`/port.rc:/dockerstartup/port.rc --writable-tmpfs --containall $1 --debug
+singularity run --bind=/home/$USER:/home/$USER/Documents,/var:/var,`pwd`/vnc.log:/dockerstartup/vnc.log,`pwd`/novnc.log:/dockerstartup/novnc.log,/tmp:/tmp,/projects:/projects,/work:/work,`pwd`/startup.sh:/dockerstartup/startup.sh,`pwd`/port.rc:/dockerstartup/port.rc --writable-tmpfs --containall $1
